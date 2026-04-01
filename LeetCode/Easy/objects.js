127479ebcdfad475538e9981bdea76a2ac17fb93
MIRNOMAN <abdullahalnoman1509@gmail.com>
2026-04-01 17:03:27 +0600

    basic fundamantal work

---
+++ LeetCode/Easy/objects.js
@@ -0,0 +1,38 @@
+// Key-value pairs. Keys are strings or symbols.
+
+// const obj = { a: 1, b: 2 };
+// console.log(obj["a"]);
+// obj["c"] = 3;
+// console.log(obj["b"]);
+// console.log(obj["c"]);
+
+// console.log(obj.a);
+// console.log(obj.b);
+
+
+
+// Similar to objects but keys can be any type, and Maps preserve insertion order.
+
+// const map = new Map();
+// map.set("a",1);
+// map.set(2, "b");
+// console.log(map.get("a"));
+// console.log(map.get(2));
+
+
+
+// Loop
+
+// const arr = [1, 2, 3];
+// // for (let i = 0; i < arr.length; i++) console.log(arr[i]);
+// for (const num of arr) console.log(num);
+
+
+// if…else
+
+//     const x = 10;
+// if (x > 5) console.log("big");
+// else console.log("small");
+
+// // Ternary
+// console.log(x > 5 ? "big" : "small"); // "big"
