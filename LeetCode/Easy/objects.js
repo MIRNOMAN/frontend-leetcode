8e5743b2e8c734b8e918ebbb8aba33565019627e
MIRNOMAN <abdullahalnoman1509@gmail.com>
2026-04-02 17:03:39 +0600

    paginations and Duplicate number

--- LeetCode/Easy/objects.js
+++ LeetCode/Easy/objects.js
@@ -36,3 +36,59 @@
 
 // // Ternary
 // console.log(x > 5 ? "big" : "small"); // "big"
+
+
+// Two Sum (Most Common Logic)  
+// Given an array, find two numbers that equal target.
+
+// const nums = [2, 7, 11, 15];
+// const target = 9 ;
+
+// const map = {}
+// for (let i = 0 ; i< nums.length; i++){
+//     const complete = target - nums[i];
+//     console.log(complete)
+//     if(map[complete]  !== undefined){
+//         console.log([map[complete], i]);
+//         return [map[complete], i];
+
+//     }
+//     map[nums[i]] = i;
+// }
+
+
+// console.log("No two sum solution", nums, target);
+
+
+
+// Example 2: Contains Duplicate
+// Check if array has duplicate numbers
+
+// const nums = [1, 2, 3, 1];
+// const set = new Set();
+// for (const num of nums) {
+//     if (set.has(num)) return true;
+//     set.add(num);
+//     console.log(num);
+// }
+// return false;   
+
+
+
+// paginations
+const items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const pageSize = 3;
+const pageNumber = 2;
+const startIndex = (pageNumber - 1) * pageSize;
+const endIndex = startIndex + pageSize;
+const page = items.slice(startIndex, endIndex);
+console.log(page);
+
+
+const itemsArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const pageSizeArray = 4;
+const pageNumberArray = 3;
+const startIndexArray = (pageNumberArray - 1) * pageSizeArray;
+const endPAgeArray = startIndexArray + pageSizeArray;
+const pageArray = (itemsArray.slice(startIndexArray, endPAgeArray));
+console.log(pageArray);
